7737fdb075ceceaa2bcce1bfe6ffaaac0d69fc47
DoThiBichCham <chamb1910037@student.ctu.edu.vn>
2023-10-03 07:12:23 -0700

    update

---
+++ mvc/views/backend/block/leftMenu.php
@@ -0,0 +1,36 @@
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+        <h3>General</h3>
+        <ul class="nav side-menu">
+            <li>
+                <a href="index.php?url=Admin/index"><i class="fa fa-tachometer" aria-hidden="true"></i></i> Home</a>
+            </li>
+            <li>
+                <a href="index.php?url=Order/index"><i class="fa fa-shopping-cart"></i> Order</a>
+            </li>
+            <li>
+                <a href="index.php?url=Category/index"><i class="fa fa-list-alt"></i> Category</a>
+            </li>
+            <li>
+                <a href="index.php?url=Product/index"><i class="fa fa-desktop"></i> Product</a>
+            </li>
+            <li>
+                <a href="index.php?url=Tags/index"><i class="fa fa-tags"></i> Tags</a> 
+            </li>
+            <li>
+                <a href="index.php?url=CategoryBlog/index"><i class="fa fa-list-alt"></i> Category of Blog </span></a>
+            </li>
+            <li>
+                <a href="index.php?url=Blog"><i class="fa fa-cloud-upload" aria-hidden="true"></i>Post Blog </a>
+            </li>
+            <li>
+                <a href="index.php?url=Slider"><i class="fa fa-picture-o" aria-hidden="true"></i> Slider</a>
+            </li>
+            <li>
+                <a href="index.php?url=User"><i class="fa fa-user" aria-hidden="true"></i></i> User</a>
+            </li>
+           
+        </ul>
+    </div>
+   
+</div>
